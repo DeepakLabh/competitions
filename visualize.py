823b0e6f6db161365a27567d72a2ac9300dc852c
DeepakLabh <labh111349@gmail.com>
2017-01-27 00:57:29 +0530

    correlate analysis

--- visualize.py
+++ visualize.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+import numpy as np
+
 __saved_context__ = {}
 #plt.rcParams['agg.path.chunksize'] = 1000
 def saveContext():
@@ -16,22 +19,40 @@ def restoreContext():
 def train_gen(data={}):
     for i in data.keys():
 	yield [i,data[i]]
-	
-train = pd.read_hdf('../kaggle_data/train.h5')
-fields = train.keys()
-#print [list(i) for i in train_gen(train)][:20]
-for i in train_gen(train):
-	try:
-		#saveContext()
-		#train = pd.read_hdf('../kaggle_data/train.h5')
-		# float(train[i][1])
-		i = list(i)
-		i[1] = map(lambda x: 0 if x==float('nan') else x,i[1])[:10000]
-		plt.plot(i[1])
-		plt.savefig('plots/'+i[0]+'.png')
-		#restoreContext()
-		plt.clf()
-		plt.close()
-	except Exception as e:
-		print e
-		pass
+
+def plot_all(data_points):	
+	fields = train.keys()
+	#print [list(i) for i in train_gen(train)][:20]
+	for i in train_gen(train):
+		try:
+			#saveContext()
+			#train = pd.read_hdf('../kaggle_data/train.h5')
+			# float(train[i][1])
+			i = list(i)
+			i[1] = map(lambda x: 0 if x==float('nan') else x,i[1])[:data_points]
+			plt.plot(i[1])
+			plt.savefig('plots/'+i[0]+'.png')
+			#restoreContext()
+			plt.clf()
+			plt.close()
+		except Exception as e:
+			print e
+			pass
+
+def cor():
+	d = {}
+	y = train.y
+	for i in train_gen(train):
+		j = list(i)
+		j[1] = map(lambda x: 0 if x==float('nan') else x,j[1])
+		c = np.correlate(y,j[1])
+		d[j[0]] = c
+		print j[0],c
+
+if __name__ == '__main__':
+	train = pd.read_hdf('../kaggle_data/train.h5')
+
+	if sys.argv[-1]=='plot':
+		plot_all(1000)
+	if sys.argv[-1] == 'cor':
+		cor()
