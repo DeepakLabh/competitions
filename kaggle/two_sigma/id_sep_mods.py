b6f888cc04bdf35ab5ab4bd3aa3dc387feecfad2
DeepakLabh <labh111349@gmail.com>
2017-02-17 00:22:33 +0530

    ids ensamble

--- kaggle/two_sigma/id_sep_mods.py
+++ kaggle/two_sigma/id_sep_mods.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 from sklearn import ensemble, linear_model
-
+from collections import Counter
 train = pd.read_hdf('../kaggle_data/train.h5')
 
-ids = train.id.unique()
-ids_sorted = sorted(ids)
+id_all = train.id
+id_count = Counter(id_all)
+ids_similar = filter(lambda x:x[1]==1813, id_count.most_common(10000))
+
+ids_un = train.id.unique()
+ids_sorted = sorted(ids_un)
 least_id = ids_sorted[-1]
 model_dict = {}
 
