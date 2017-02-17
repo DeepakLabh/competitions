297c1faef961c9e9822907e78ff753663696eab3
Deepak Labh <labh111349@gmail.com>
2017-02-17 17:50:35 +0530

    timer function

--- kaggle/two_sigma/id_sep_mods.py
+++ kaggle/two_sigma/id_sep_mods.py
@@ -3,20 +3,46 @@ import numpy as np
 # import matplotlib.pyplot as plt
 from sklearn import ensemble, linear_model
 from collections import Counter
-train = pd.read_hdf('../kaggle_data/train.h5')
 
-id_all = train.id
-id_count = Counter(id_all)
-ids_similar = map(lambda y: y[0], filter(lambda x:x[1]==1813, id_count.most_common(10000)))
-ids_similar_value = 1813 ## no of rows in all similar retrieved values.
+import time
+from functools import wraps
+from memory_profiler import profile
+ 
+ 
+############# decorator function of time ######################
+def fn_timer(function):
+    @wraps(function)
+    def function_timer(*args, **kwargs):
+        t0 = time.time()
+        result = function(*args, **kwargs)
+        t1 = time.time()
+        print ("Total time running %s: %s seconds" %
+               (function.func_name, str(t1-t0))
+               )
+        return result
+    return function_timer
+############# decorator function of time ######################
+@profile
+def essentials():
+	train = pd.read_hdf('../kaggle_data/train.h5')
 
-ids_un = train.id.unique()
-ids_sorted = sorted(ids_un)
-least_id = ids_sorted[-1]
-model_dict = {}
+	id_all = train.id
+	id_count = Counter(id_all)
+	ids_similar = map(lambda y: y[0], filter(lambda x:x[1]==1813, id_count.most_common(10000)))
+	ids_similar_value = 1813 ## no of rows in all similar retrieved values.
 
-train_mean = train.mean
+	ids_un = train.id.unique()
+	ids_sorted = sorted(ids_un)
+	least_id = ids_sorted[-1]
+	model_dict = {}
 
+	train_mean = train.mean(axis = 0)
+	important_features = ['technical_20', 'technical_30']
+	train.fillna(train_mean, inplace=True)
+	for i in important_features:
+		train[i+'diff'] = train[i] - train[i].shift(1)
+		important_features.append(i+'diff')
+	
 # cols_train = ['technical_20', 'technical_30', ]
 class Features:
 
@@ -31,5 +57,6 @@ class Features:
 	def predict(self, x):
 		return self.model.predict(x)
 
-a = Features(ids_similar)
-print(a)
+essentials()
+# a = Features(ids_similar)
+# print(a)
