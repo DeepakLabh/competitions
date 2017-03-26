cdfd397262385eb667dd219f5f78f45073b3d6e6
Deepak Labh <labh111349@gmail.com>
2017-03-26 15:21:28 +0530

    vec mongo

--- kaggle/quora/utils.py
+++ kaggle/quora/utils.py
@@ -12,13 +12,14 @@ def create_vocab(filename):
     fields = ['question1', 'question2']
     data_q = data[fields]
     data_q_array = np.array(data_q).flatten()
-    data_q_splitted = map(lambda x: re.split(r'\s\W*|\W*$', x) if isinstance(x,str) else x,data_q_array)
+    data_q_splitted = map(lambda x: re.split(r'\W*', x) if isinstance(x,str) else '',data_q_array)
     vocab_all = []
     for i in data_q_splitted: 
         try:
 	    vocab_all+=i 
 	except Exception as e: print e,i
-    vocab_all = list(set(vocab_all))
+    vocab_all = map(lambda x: x.lower(), list(set(vocab_all)))
+    
     return vocab_all
 
 def create_trainable_for_vector(filenames):
@@ -30,7 +31,7 @@ def create_trainable_for_vector(filenames):
 	data_q_array = np.array(data_q).flatten()
 	data_q_array = map(lambda x: re.sub(r'\W*$',' ',x) if isinstance(x, str) else '', data_q_array)
 	data_all += data_q_array
-    data = ' '.join(data_all)
+    data = ' '.join(data_all).lower()
     return data
 
 def write_vectors(binary_file_path, vocab_file_path, client):
@@ -45,4 +46,10 @@ def write_vectors(binary_file_path, vocab_file_path, client):
 
 if __name__ == '__main__':
     client = MongoClient()
-    write_vectors('../quora_data/quora-vector.bin', '../quora_data/vocab.json', client)
+    if sys.argv[-1] == 'create_vocab':
+        d1= create_vocab('../quora_data/train.csv')
+        d2= create_vocab('../quora_data/test.csv')
+        d = list(set(d1+d2))
+        json.dump(d, open('../quora_data/vocab.json','w'))
+    if sys.argv[-1] == 'index_vectors':
+        write_vectors('../quora_data/quora-vector.bin', '../quora_data/vocab.json', client)
