046c854239214ff11aa15b430a4be13f1f04880d
Deepak Labh <labh111349@gmail.com>
2017-04-01 10:59:29 +0530

    index training data

--- kaggle/quora/train.py
+++ kaggle/quora/train.py
@@ -4,74 +4,29 @@ import numpy as np
 import model_arch as ma
 import re
 
-import pickle as pk
-import gzip
+#import pickle as pk
+#import gzip
 
 c = MongoClient()
 coll_vec = c.data.wordvec
-wordvec_dim = 200
-###################################### LOAD TRAINING DATA OR CREATE ###########################################
-try:
-    x1,x2 = pk.load(gzip.open('../quora_data/training_data.zip', 'rb'))
-    print 'Compressed Pickle loading done'
-except Exception as pickle_load_error:
-    print pickle_load_error, 'Creating training data'
-    #print 'load training data'
-    data = pd.read_csv('../quora_data/train.csv')
-    y = data['is_duplicate']
-    x1 = data['question1']
-    x2 = data['question2']
-    
-    x1 = map(lambda x: re.split(r'\W*', x.lower())[:-1] if isinstance(x, str) else '', x1)
-    x2 = map(lambda x: re.split(r'\W*', x.lower())[:-1] if isinstance(x, str) else '', x2)
-    
-    
-    max_sent_len = max([max(map(lambda x: len(x), x1)), max(map(lambda x: len(x), x2))])
-    max_sent_len = 60
-    
-    print 'create trainable data'
-    #x1 = map(lambda x: map(lambda xx: coll_vec.find_one({'word':xx})['vec'] if coll_vec.find_one({'word':xx})!=None else np.zeros(wordvec_dim), x), x1)
-    #x2 = map(lambda x: map(lambda xx: coll_vec.find_one({'word':xx})['vec'] if coll_vec.find_one({'word':xx})!=None else np.zeros(wordvec_dim), x), x2)
-    
-    x1_train = []
-    for i in xrange(len(x1)):
-        #print 'hello111111111',i
-        x1_sents = []
-        for j in xrange(max_sent_len):
-            try:
-                if j>=len(x1[i]):
-                    x1[i].append(np.zeros(wordvec_dim))
-                else:
-        		#print 'hello1222222222222', j
-                    x1[i][j] = coll_vec.find_one({'word':x1[i][j]})['vec']
-            except:
-                if j>=len(x1[i]):
-                    x1[i].append(np.zeros(wordvec_dim))
-                else:
-                    x1[i][j] = np.zeros(wordvec_dim)
+question1_collection = c.data.question1
+question2_collection = c.data.question2
+
+def batch_gen_2d(batch_size):
+    while True:
+        i = randint(start_index, end_index)
+        y_batch = np.array(y_train[i:i+batch_size])
+        x_batch = ([x_train[k-input_shape[0]+1:k+1] for k in range(i, i+batch_size)]) #  Create 2-D input
+        # x_batch = np.array(x_batch)
+        # print (len(x_batch),33333333333, len(x_batch[0]))
+        # x_batch = ([[x_train[k-j:k+1] for j in range(10,0,-1)] for k in range(i, i+batch_size-1)])
+        # print (len(x_batch), len(x_batch[0]), len(x_batch[0].keys())),3333333333333333
+        x_batch = map(lambda x: np.reshape(np.array(x), (input_shape[0], x_train.shape[1])),x_batch) # 10 is nu of recurrent layers
+        # print (x_batch.shape,4444444444444444)
         
-        try: 1/(i%10000)
-        except: print i
-    
-    for i in xrange(len(x2)):
-        for j in xrange(max_sent_len):
-            try:
-                if j>=len(x2[i]):
-                    x2[i].append(np.zeros(wordvec_dim))
-                else:
-                    x2[i][j] = coll_vec.find_one({'word':x2[i][j]})['vec']
-            except:
-                if j>=len(x2[i]):
-            	    x2[i].append(np.zeros(wordvec_dim))
-                else:
-                    x2[i][j] = np.zeros(wordvec_dim)
-        try: 1/(i%10000)
-        except: print i
-    
-    try:
-        pk.dump([x1,x2], gzip.open('../quora_data/training_data.zip', 'wb'), -1)
-        print 'Compressed pickling done'
-    except Exception as e:
-        print e, 'pickling error'
-        pass
-###################################### LOAD TRAINING DATA OR CREATE ###########################################
+        x_batch = np.array(list(x_batch))
+        
+
+        yield (x_batch, y_batch)
+        # break
+
--- kaggle/quora/utils.py
+++ kaggle/quora/utils.py
@@ -42,6 +42,46 @@ def write_vectors(binary_file_path, vocab_file_path, client):
 	    client.data.wordvec.insert({'vec': word_vectors[i].tolist(), 'word':i})
 	except Exception as e:
 	    print e
+def index_training_data():
+    print 'create trainable data'
+    #x1 = map(lambda x: map(lambda xx: coll_vec.find_one({'word':xx})['vec'] if coll_vec.find_one({'word':xx})!=None else np.zeros(wordvec_dim), x), x1)
+    #x2 = map(lambda x: map(lambda xx: coll_vec.find_one({'word':xx})['vec'] if coll_vec.find_one({'word':xx})!=None else np.zeros(wordvec_dim), x), x2)
+    x1_train = []
+    for i in xrange(len(x2)):
+        #print 'hello111111111',i
+        x1_sents = []
+        for j in xrange(max_sent_len):
+            try:
+                if j>=len(x1[i]):
+                    x1_sents.append(np.zeros(wordvec_dim))
+                else:
+                        #print 'hello1222222222222', j
+                    x1_sents.append(coll_vec.find_one({'word':x1[i][j]})['vec'])
+            except:
+                #if j>=len(x1[i]):
+                x1_sents.append(np.zeros(wordvec_dim))
+               # else:
+               #     x1[i][j] = np.zeros(wordvec_dim)
+        question1_collection.insert_one({'_id':i, 'vec':np.array(x1_sents).tolist()})
+        try: 1/(i%10000)
+        except: print i
+
+    for i in xrange(len(x2)):
+        x2_sents = []
+        for j in xrange(max_sent_len):
+            try:
+                if j>=len(x2[i]):
+                    x2_sents.append(np.zeros(wordvec_dim))
+                else:
+                    x2_sents.append(coll_vec.find_one({'word':x2[i][j]})['vec'])
+            except:
+                if j>=len(x2[i]):
+                    x2_sents.append(np.zeros(wordvec_dim))
+                #else:
+                #    x2[i][j] = np.zeros(wordvec_dim)
+        question2_collection.insert_one({'_id':i, 'vec':np.array(x2_sents).tolist()})
+        try: 1/(i%10000)
+        except: print i
 
 
 if __name__ == '__main__':
@@ -53,3 +93,28 @@ if __name__ == '__main__':
         json.dump(d, open('../quora_data/vocab.json','w'))
     if sys.argv[-1] == 'index_vectors':
         write_vectors('../quora_data/quora-vector.bin', '../quora_data/vocab.json', client)
+
+    if sys.argv[-1]=='index_train':
+        import pandas as pd
+        from pymongo import MongoClient
+        import numpy as np
+        import re
+        
+        c = MongoClient()
+        coll_vec = c.data.wordvec
+        question1_collection = c.data.question1
+        question2_collection = c.data.question2
+        wordvec_dim = 200
+        data = pd.read_csv('../quora_data/train.csv')
+        y = data['is_duplicate']
+        x1 = data['question1']
+        x2 = data['question2']
+        
+        x1 = map(lambda x: re.split(r'\W*', x.lower())[:-1] if isinstance(x, str) else '', x1)
+        x2 = map(lambda x: re.split(r'\W*', x.lower())[:-1] if isinstance(x, str) else '', x2)
+        
+        
+        max_sent_len = max([max(map(lambda x: len(x), x1)), max(map(lambda x: len(x), x2))])
+        max_sent_len = 60
+
+        index_training_data()
