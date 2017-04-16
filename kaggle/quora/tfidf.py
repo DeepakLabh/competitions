64e206115c8c464106c3ff3c439e2182f74f5d95
Deepak <labh111349@gmail.com>
2017-04-16 11:48:37 +0530

    tfidf added

--- kaggle/quora/tfidf.py
+++ kaggle/quora/tfidf.py
@@ -1,10 +1,32 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
+import math
+import re
 
 class tfidf(object):
     def __init__(self, corpus_file_path):
-	self.corpus_dict = corpus_file_path
-	self.corpus_dict = open(corpus_file_path).read().lower().split('')
+        self.corpus_file_path = corpus_file_path
+        self.corpus_list = re.split(r'\W*', open(corpus_file_path).read().lower())
+        self.corpus_length = len(self.corpus_list)
+	self.corpus_dict = Counter(self.corpus_list)
+
+    def tf(self, word, sent):
+        word_list = re.split(r'\W*', sent.lower())
+        word_count = Counter(word_list)
+        t = word_count[word]
+        return t
+
+    def idf(self, word, sent):
+        try:
+            #print  self.corpus_dict[word],111111,self.corpus_length,float(self.corpus_dict[word]/self.corpus_length)
+            i = abs(math.log(float(self.corpus_dict[word])/self.corpus_length))
+        except Exception as e:
+            print e, 'could not make tfidf vector'
+            i = float(1/self.tf(word, sent))
+        return i
 
     def vec(self, sent):
+        word_list = re.split(r'\W*', sent.lower())
+        vect = map(lambda word: self.tf(word, sent)*self.idf(word, sent), word_list)
+        return vect
--- kaggle/quora/train.py
+++ kaggle/quora/train.py
@@ -21,7 +21,7 @@ wordvec_dim = 200
 gru_output_dim = 50
 output_dim = 2
 train_size = 500
-batch_size = 40
+batch_size = 100
 num_epoch = 100
 def batch_gen_2d(batch_size, start_index, end_index):
     while True:
@@ -30,16 +30,18 @@ def batch_gen_2d(batch_size, start_index, end_index):
 	#i = 1
 	#print '    ',i,' iiiiiiiiiii2'
         y_batch = np.array(map(lambda x: np.array([1,0] if x==1 else [0,1]),y[i:i+batch_size]))
-        x_batch2 = map(lambda x: np.array(x['vec'][:max_sent_len]), list(question2_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))	
-        x_batch1 = map(lambda x: np.array(x['vec'][:max_sent_len]), list(question1_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))	
+        x_batch2 = map(lambda x: np.array(x['vec'][:max_sent_len]), list(question2_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))
+        x_batch2_tf = map(lambda x: np.array(x['tf'][:max_sent_len] if len(x['tf'])>= max_sent_len else x['tf']+list(np.zeros(max_sent_len-len(x['tf'])))), list(question2_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))
+        x_batch1 = map(lambda x: np.array(x['vec'][:max_sent_len]), list(question1_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))
+        x_batch1_tf = map(lambda x: np.array(x['tf'][:max_sent_len] if len(x['tf'])>= max_sent_len else x['tf']+list(np.zeros(max_sent_len-len(x['tf'])))), list(question1_collection.find({'_id': {'$gte': i, '$lt': i+batch_size}})))
         #x_batch = map(lambda x: np.reshape(np.array(x), (input_shape[0], x_train.shape[1])),x_batch) # 10 is nu of recurrent layers
-        ############ To normalize the inputs ############3 
+        ############ To normalize the inputs ############3
         x_batch1 = map(lambda x: map(lambda xx: abs(xx)-abs(x.mean()), x), x_batch1)
         x_batch2 = map(lambda x: map(lambda xx: abs(xx)-abs(x.mean()), x), x_batch2)
-        ############ To normalize the inputs ############3 
-        x_batch1 = np.array(list(x_batch1))
-        x_batch2 = np.array(list(x_batch2))
-        #print y_batch,111111111111111 
+        ############ To normalize the inputs ############3
+        x_batch1 = np.array(list(x_batch1))*np.array(x_batch1_tf)
+        x_batch2 = np.array(list(x_batch2))*np.array(x_batch2_tf)
+        #print y_batch,111111111111111
         yield [x_batch1, x_batch2], y_batch
         # break
 
@@ -48,11 +50,11 @@ model = ma.dense_test(max_sent_len, wordvec_dim, gru_output_dim, output_dim)
 print 'fitting model ...'
 
 #fit = model.fit_generator(generator=batch_gen_2d(500,1,10000), nb_epoch=30, samples_per_epoch=train_size)
-history = model.fit_generator(generator=batch_gen_2d(batch_size,1,1000), nb_epoch = num_epoch, validation_data = batch_gen_2d(batch_size,11000,12010), nb_val_samples = 100, samples_per_epoch=train_size)
+history = model.fit_generator(generator=batch_gen_2d(batch_size,1,1000), nb_epoch = num_epoch, validation_data = batch_gen_2d(batch_size,9000,9510), nb_val_samples = 100, steps_per_epoch=50)
 
 plot_model(model, to_file='model.png')
 
-model.save_weights("../quora_data/siamese_lstm.weights",overwrite=True) 
+model.save_weights("../quora_data/siamese_lstm.weights",overwrite=True)
 
 #########################3 PLOT loss and accuracy ##############
 # summarize history
