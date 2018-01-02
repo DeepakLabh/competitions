b206ba29851c2fe9170463cdb1c785608a9abbbd
Deepak Labh <labh111349@gmail.com>
2018-01-02 16:40:13 +0530

    transfer learning to XGBOOST

--- kaggle/grocery/keras_model_all.py
+++ kaggle/grocery/keras_model_all.py
@@ -7,6 +7,7 @@ import sys
 import pandas as pd
 import numpy as np
 import random
+import xgboost as xgb
 # import utils as vs
 
 num_items = 3127115
@@ -33,12 +34,18 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 
-hol = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/holidays_events.csv')
 # tra = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/train_100000.csv')
+hol = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/holidays_events.csv')
 itm = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/items.csv')
 st = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/stores.csv')
 oil = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/oil.csv')
 trans = pd.read_csv('/home/arya/deepak/competitions/kaggle/grocery/data/transactions.csv')
+
+# hol = pd.read_csv('../input/holidays_events.csv')
+# itm = pd.read_csv('../input/items.csv')
+# st = pd.read_csv('../input/stores.csv')
+# oil = pd.read_csv('../input/oil.csv')
+# trans = pd.read_csv('../input/transactions.csv')
 
 set_hol = array(list(set(map(lambda x,y:str(x)+str(y), hol['type'], hol['locale']))))
 set_itm = array(list(set(map(lambda x:str(x), itm['family']))))
@@ -68,7 +75,6 @@ def prepare_cat_data(data):
     cat = list(map(lambda x: np.array(x), cat))
     #mer['cat'] = mer['cat'].apply(lambda x: array(x))
     #print np.asarray(cat['cat']),111111111111111111111111
-    print (array(cat).shape)
     return np.array(cat)
     
 
@@ -100,6 +106,9 @@ def prepare_data(data):
 def acc_loss(y_true, y_pred):
     rms =  K.sqrt(K.mean(K.square(y_true - y_pred)))
     return rms
+def nrms(y_true, y_pred):
+    rms = np.sqrt(np.mean(np.square(y_true - y_pred)))
+    return rms
     
 def r2_keras(y_true, y_pred):
     SS_res =  K.sum(K.square(y_true - y_pred)) 
@@ -123,6 +132,7 @@ def fcnn(input_dim1, input_dim2, input_dim3, input_dim4):
     f1 = Activation('relu')(f1)
     f1 = Dense(100)(f1)
     f1 = Activation('relu')(f1)
+    # f1 = Dropout(0.2)(f1)
     
     e2 = Dense(30)(input_layer2)
     f2 = Activation('relu')(e2)
@@ -130,6 +140,7 @@ def fcnn(input_dim1, input_dim2, input_dim3, input_dim4):
     f2 = Activation('relu')(e2)
     f2 = Dense(200)(f2)
     f2 = Activation('relu')(f2)
+    # f2 = Dropout(0.2)(f2)
     f2 = Dense(15)(f2)
     f2 = Activation('relu')(f2)
     # f2 = Dense(5)(f2)
@@ -140,6 +151,7 @@ def fcnn(input_dim1, input_dim2, input_dim3, input_dim4):
     d3 = Dense(90)(input_layer3)
     d3 = Activation('relu')(d3)
     d3 = Dense(500)(d3)
+    # d3 = Dropout(0.20)(d3)
     d3 = Activation('relu')(d3)
     d3 = Dense(10)(d3)
     d3 = Activation('relu')(d3)
@@ -151,8 +163,9 @@ def fcnn(input_dim1, input_dim2, input_dim3, input_dim4):
     c1 = Activation('relu')(c1)
     c1 = Dense(500)(c1)
     c1 = Activation('relu')(c1)
-    c1 = Dense(50)(c1)
-    c1 = Activation('relu')(c1)
+    # c1 = Dropout(0.15)(c1)
+    # c1 = Dense(50)(c1)
+    # c1 = Activation('relu')(c1)
 
     do = keras.layers.concatenate([f1, f2, d3, c1], axis=-1)
     do = Dense(100)(do)
@@ -168,6 +181,16 @@ def fcnn(input_dim1, input_dim2, input_dim3, input_dim4):
     model.compile(loss = 'mae', optimizer='adam', metrics = [acc_loss])
     return model
 
+def get_model_layer_weights(model, out_layer_index, input_data):
+    in_layers = model.input
+    in_layers.append((K.learning_phase()))
+    out_layers = model.layers[-3]
+    func = K.function(in_layers, [out_layers.output])
+    outs = func(input_data)
+    outs = np.array(outs)
+    #print (outs.shape)
+    outs = outs.reshape((outs.shape[1], outs.shape[2]))
+    return outs
     
 if __name__ == "__main__":
     ####################################################################################
@@ -176,8 +199,11 @@ if __name__ == "__main__":
     ####################################################################################################################################
     
     CHUNKSIZE = 120000
+    TEST_CHUNKSIZE = 1200000
     NUM_OF_ITERATIONS = 100
     BATCH_SIZE = 1000
+    TRAIN_SIZE = 92000000
+    STOP_DL_TRAIN_CHUNK_COUNT = 100
     if sys.argv[-1] == 'dev':
         TEST_PATH = 'data/test.csv'
         TRAIN_PATH = 'data/train.csv'
@@ -185,36 +211,41 @@ if __name__ == "__main__":
         TEST_PATH = '../input/test.csv'
         TRAIN_PATH = '../input/train.csv'
     
-    test = pd.read_csv(TEST_PATH, nrows= 10)
+    ################## XGB Parameters ##########################################
+    param = {
+        'max_depth': 10,  # the maximum depth of each tree
+        'eta': 0.3,  # the training step for each iteration
+        'silent': 1,  # logging mode - quiet
+        "objective": "reg:linear", 
+        "booster":"gblinear",  # error evaluation for multiclass training
+        }  # the number of classes that exist in this datset
+    num_round = 20  # the number of training iterations
+    ################## XGB Parameters ##########################################
+    
     # vis_data = pd.read_csv(TRAIN_PATH, nrows = 100)
     # vis_cat_data = prepare_cat_data(vis_data)
     # x_vis_data, y_vis_data = prepare_data(vis_data)
     # x_vis_data.append(vis_cat_data)
+    train_all = pd.read_csv(TRAIN_PATH, skiprows = range(1, 101688779))
     chunk_count=0
-    for train in pd.read_csv(TRAIN_PATH, chunksize=CHUNKSIZE):#, skiprows = range(1, 125397041))
+    # for train in pd.read_csv(TRAIN_PATH, chunksize=CHUNKSIZE):#, skiprows = range(1, 125397041))
+    for i_t in range(0, TRAIN_SIZE, CHUNKSIZE):
+        train = train_all.loc[i_t:i_t+CHUNKSIZE]
+        print (i_t, i_t+CHUNKSIZE, train.shape, train_all.shape,1111111111111111111111111)
         chunk_count+=1
         
     
         train.loc[train.unit_sales < 0, 'unit_sales'] = 0
         train['date'] = pd.to_datetime(train['date'])
-        test['date'] = pd.to_datetime(test['date'])
+        
         train['year'] = train['date'].dt.year
-        test['year'] = test['date'].dt.year
-        
         train['sin_month'] = np.sin(2*np.pi*train['date'].dt.month/12)
-        test['sin_month'] = np.sin(2*np.pi*test['date'].dt.month/12)
-        test['sin_day'] = np.sin(2*np.pi*test['date'].dt.day/31)
         train['sin_day'] = np.sin(2*np.pi*train['date'].dt.day/31)
-        test['sin_dayofweek'] = np.sin(2*np.pi*test['date'].dt.dayofweek/7)
         train['sin_dayofweek'] = np.sin(2*np.pi*train['date'].dt.dayofweek/7)
-        
         train['cos_month'] = np.cos(2*np.pi*train['date'].dt.month/12)
-        test['cos_month'] = np.cos(2*np.pi*test['date'].dt.month/12)
-        test['cos_day'] = np.cos(2*np.pi*test['date'].dt.day/31)
         train['cos_day'] = np.cos(2*np.pi*train['date'].dt.day/31)
-        test['cos_dayofweek'] = np.cos(2*np.pi*test['date'].dt.dayofweek/7)
         train['cos_dayofweek'] = np.cos(2*np.pi*train['date'].dt.dayofweek/7)
-        
+
         train['unit_sales'] =  train['unit_sales'].apply(pd.np.log1p) #logarithm conversion
         
         item_nbr_bin = train['item_nbr'].apply(lambda x: [i for i in bin(x)[2:]])
@@ -224,25 +255,39 @@ if __name__ == "__main__":
         store_nbr_bin = keras.preprocessing.sequence.pad_sequences(store_nbr_bin, maxlen=10, dtype='int32', padding='pre', truncating='pre', value=0.)
         categoricals_train = prepare_cat_data(train)
         
+        
         print ('train, test Data loaded ...')
-        print ('test shape: ', test.shape, 'train shape: ', train.shape)
+        print ('train shape: ', train.shape)
         #test_all = pd.merge(test, train, how = 'left', on = ['item_nbr','store_nbr', 'dayofweek', 'month', 'date'])
         
         itr = 0
         for ri in random.sample(range(100, CHUNKSIZE-10000), NUM_OF_ITERATIONS):
             itr += 1
             # [id,date,store_nbr,item_nbr,unit_sales,onpromotion]
-            #print categoricals[ri: ri+BATCH_SIZE].shape
+            # print (categoricals_train[ri: ri+BATCH_SIZE].shape, 222222222222222)
             x_train = [np.array(train.drop(['id','date','unit_sales','onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)[ri:ri+BATCH_SIZE]), item_nbr_bin[ri:ri+BATCH_SIZE], store_nbr_bin[ri:ri+BATCH_SIZE], categoricals_train[ri: ri+BATCH_SIZE]]
             y_train = np.array(train['unit_sales'][ri:ri+BATCH_SIZE])
             y_valid = np.array(train['unit_sales'][-1000:])
             x_valid = [np.array(train.drop(['id','date','unit_sales','onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)[-1000:]), item_nbr_bin[-1000:], store_nbr_bin[-1000:], categoricals_train[-1000:]]
             
-            train_logs = model.train_on_batch(x_train, y_train)
-            valid_logs = model.test_on_batch( x_valid, y_valid)
+            if chunk_count < STOP_DL_TRAIN_CHUNK_COUNT:
+                train_logs = model.train_on_batch(x_train, y_train)
+                valid_logs = model.test_on_batch( x_valid, y_valid)
             if (itr%20==0):
                 print ('====================== CHUNK NO: ', chunk_count, 'ITERAITON: ', itr, ' ================================')
-                print ('Train log: ', train_logs, '\nTest log: ', valid_logs)
+                if chunk_count < STOP_DL_TRAIN_CHUNK_COUNT:
+                    print ('Train log: ', train_logs, '\nTest log: ', valid_logs)
+                if chunk_count > STOP_DL_TRAIN_CHUNK_COUNT:
+                    dl_weights_train = get_model_layer_weights(model, -3, x_train)
+                    dl_weights_valid = get_model_layer_weights(model, -3, x_valid)
+                    x_xgb = np.concatenate((np.array(train.drop(['id','date','unit_sales','onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)[ri:ri+BATCH_SIZE]), item_nbr_bin[ri:ri+BATCH_SIZE], store_nbr_bin[ri:ri+BATCH_SIZE], categoricals_train[ri: ri+BATCH_SIZE], dl_weights_train), axis=1)
+                    #print (x_xgb.shape, 11111111111111)
+                    x_xgb = xgb.DMatrix(x_xgb, label = y_train)
+                    bst = xgb.train(param, x_xgb, num_round)
+                    x_xgb_valid = np.concatenate((np.array(train.drop(['id','date','unit_sales','onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)[-1000:]), item_nbr_bin[-1000:], store_nbr_bin[-1000:], categoricals_train[-1000:], dl_weights_valid), axis=1)
+                    x_xgb_valid = xgb.DMatrix(x_xgb_valid, label = y_valid)
+                    y_xgv_valid_pred = bst.predict(x_xgb_valid)
+                    print ("XGB VALIDATION LOSS: %f ---->>" %(nrms(y_valid, y_xgv_valid_pred)))
             
                 y_valid_pred = model.predict(x_valid)
                 # print ("standard deviation : %f , Mean : %f of training chunk ---->>" %(y_train.std(), y_train.mean()))
@@ -250,21 +295,36 @@ if __name__ == "__main__":
                 # vis_predict_data = model.predict(x_vis_data)
                 # vis2(y_vis_data, vis_predict_data)
                 print ("standard deviation : %f , Mean : %f of Predicted Validating chunk ---->>" %(y_valid_pred.std(), y_valid_pred.mean()))
-        if chunk_count >2: break
+        if chunk_count >1000: break
     
+    df_test = pd.DataFrame(columns=["id",'unit_sales'])
+    for test in pd.read_csv(TEST_PATH, chunksize = TEST_CHUNKSIZE):#, nrows= 10)
     #save_model(model, "model.h5")
-    test_item_nbr_bin = test['item_nbr'].apply(lambda x: [i for i in bin(x)[2:]])
-    test_item_nbr_bin = keras.preprocessing.sequence.pad_sequences(test_item_nbr_bin, maxlen=25, dtype='int32', padding='pre', truncating='pre', value=0.)
+        test['date'] = pd.to_datetime(test['date'])
+        test['year'] = test['date'].dt.year
+        test['sin_month'] = np.sin(2*np.pi*test['date'].dt.month/12)    
+        test['sin_day'] = np.sin(2*np.pi*test['date'].dt.day/31)
+        test['sin_dayofweek'] = np.sin(2*np.pi*test['date'].dt.dayofweek/7)
+        test['cos_month'] = np.cos(2*np.pi*test['date'].dt.month/12)
+        test['cos_day'] = np.cos(2*np.pi*test['date'].dt.day/31)
+        test['cos_dayofweek'] = np.cos(2*np.pi*test['date'].dt.dayofweek/7)
+    
+        test_item_nbr_bin = test['item_nbr'].apply(lambda x: [i for i in bin(x)[2:]])
+        test_item_nbr_bin = keras.preprocessing.sequence.pad_sequences(test_item_nbr_bin, maxlen=25, dtype='int32', padding='pre', truncating='pre', value=0.)
         
-    test_store_nbr_bin = test['store_nbr'].apply(lambda x: [i for i in bin(x)[2:]])
-    test_store_nbr_bin = keras.preprocessing.sequence.pad_sequences(test_store_nbr_bin, maxlen=10, dtype='int32', padding='pre', truncating='pre', value=0.)
-    categoricals_test = prepare_cat_data(test)
-
-    y_pred = model.predict([np.array(test.drop(['id','date', 'onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)),test_item_nbr_bin, test_store_nbr_bin, categoricals_test])
-    # y_pred = y_pred.apply(pd.np.expm1)
-    sub_dic = {'id': test['id'], 'unit_sales': y_pred.reshape(len(test))}
-    df = pd.DataFrame(sub_dic, columns=["id",'unit_sales'])
-    df['unit_sales'] = df['unit_sales'].apply(pd.np.expm1)
-    # df.to_csv('keras_submission.csv', index=False)
-    print ("standard deviation : %f , Mean : %f of Test chunk" %(df['unit_sales'].std(), df['unit_sales'].mean()))
-    print (df.head())
+        test_store_nbr_bin = test['store_nbr'].apply(lambda x: [i for i in bin(x)[2:]])
+        test_store_nbr_bin = keras.preprocessing.sequence.pad_sequences(test_store_nbr_bin, maxlen=10, dtype='int32', padding='pre', truncating='pre', value=0.)
+        # print ('test data prepared ...111')
+        categoricals_test = prepare_cat_data(test)
+        print ('test data prepared ...')
+        y_pred = model.predict([np.array(test.drop(['id','date', 'onpromotion', 'item_nbr','store_nbr', 'year'], axis=1)),test_item_nbr_bin, test_store_nbr_bin, categoricals_test])
+        print ('results predicted ...')
+        sub_dic = {'id': test['id'], 'unit_sales': y_pred.reshape(len(test))}
+        df = pd.DataFrame(sub_dic, columns=["id",'unit_sales'])
+        df['unit_sales'] = df['unit_sales'].apply(pd.np.expm1)
+        
+        print ("standard deviation : %f , Mean : %f of Test chunk" %(df['unit_sales'].std(), df['unit_sales'].mean()))
+        df_test = pd.concat([df, df_test])
+        print ('test shape till now : ', df_test.shape)
+    # print (df.head())
+    df_test.to_csv('keras_submission.csv', index=False)
